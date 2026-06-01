599b910aa4505692c74a2fa85204c8847c8430da
Erinala Horlbeck <erinala.horlbeck@wavesycle.com>
2026-06-01 12:53:45 +0200

    Update website: Mon Jun  1 12:53:45 CEST 2026

---
+++ _nuxt/C9BXdf4O.js
@@ -0,0 +1,1 @@
+import"./t_ENeAx0.js";const e=()=>({legacy:!1,locale:"en",fallbackLocale:"en"});export{e as default};
---
+++ _nuxt/CTyoMMOG.js
@@ -0,0 +1,1 @@
+import{c as e,g as t}from"./t_ENeAx0.js";const _={__name:"empty",setup(c){return(r,a)=>(t(),e("div"))}};export{_ as default};
