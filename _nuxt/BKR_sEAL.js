08da29a278c716618e3d5eae493e64230785a7b5
Erinala Horlbeck <erinala.horlbeck@wavesycle.com>
2026-09-25 15:40:39 +0200

    Update website: Fri Sep 25 15:40:39 CEST 2026

---
+++ _nuxt/BKR_sEAL.js
@@ -0,0 +1,1 @@
+import"./dukiRg9I.js";const e=()=>({legacy:!1,locale:"en",fallbackLocale:"en"});export{e as default};
---
+++ _nuxt/C4pA5o3X.js
@@ -0,0 +1,1 @@
+import{h as e,c as t}from"./dukiRg9I.js";const _={__name:"empty",setup(c){return(r,a)=>(e(),t("div"))}};export{_ as default};
